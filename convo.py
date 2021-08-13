75bcc4a7674c9035b5fb3dba71803625377aad31
Thomas Findlay <findlaytel@gmail.com>
2021-08-13 23:47:34 +0100

    Built out functionality
    Added:
    norm layer type
    get_dim_sizes
    stripe search proto
    
    Fixed:
    norm layer type
    
    TBD:
    improve solver as per comments
    _parse_kernel_size

--- convo.py
+++ convo.py
@@ -39,23 +39,46 @@ def make_convolutions(in_shape, out_shape, n_layers, kernel_size=None, stride=1,
     
     _check_shapes(in_shape, out_shape)
     
+    kernel_size = _parse_kernel_size(kernel_size) ## TBD
+    
     layers = _LastUpdatedOrderedDict()
-    channel_nums = _get_channel_dist(in_shape, out_shape, n_layers)
-    n_dims = len(in_shape) - 1
+   
+    n_dims = len(in_shape) - 1 # SPATIAL dims
+    
+    dim_shapes = np.zeros((n_layers, n_dims))
+    for dim in range(n_dims + 1):
+        shapes[:, dim] = np.array(_get_dim_sizes(in_shape[dim], out_shape[dim], n_layers))
     
     conv_layer_type = _get_conv_layer_type(n_dims)
     pool_layer_type = _get_pool_layer_type(pool_type, n_dims)
-    norm_layer_type = ....
+    norm_layer_type = _get_norm_layer_type(norm_type, n_dims)
     
-    for n in range(1, n_layers + 1):
-        layers[f"conv_{n}"] = conv_layer_type() ## TBD
-        layers[f"pool_{n}"] = pool_layer_type()
+    for n in range(n_layers):
+        layers[f"conv_{n + 1}"] = conv_layer_type()
+        layers[f"pool_{n + 1}"] = pool_layer_type()
         if norm_type is not None:
-            layers[f"{norm_layer_type}_norm_{n}"]
+            layers[f"{norm_layer_type}_norm_{n + 1}"]
     
     
     ### Allow nn.ModuleList output too??
     return nn.Sequential(layers)
+
+def _stripe_search_indices(n, n_params):
+    if length == 1:
+        yield (n,)
+        return
+
+    for i in range(n + 1):
+        for t in get_tuples(n_params - 1, n - i):
+            yield (i,) + t
+  
+def _find_layer_params(in_shape, out_shape, dilation):
+    for n in range(8):  # TBD make it per dim, make it accept user defined values
+        for padding, kernel, stride in _stripe_search_indices(n, 4):
+            if map(lambda x: _size_fcn(x, padding, dilation, kernel + 1, stride + 1), in_shape) == out_shape:
+                return padding, kernel + 1, stride + 1
+    
+    raise RuntimeError("Was not able to find suitable parameters for given inputs!")
     
 def _get_conv_layer_type(n_dims):
     return (nn.Conv1d, nn.Conv2d, nn.Conv3d)[n_dims - 1]
@@ -68,7 +91,7 @@ def _get_pool_layer_type(pool_type, n_dims):
     else:
         raise ValueError(f"Got pooling layer type '{pool_type}' not 'max' or 'avg'.")
     
-def _get_norm_layer_type(norm_type):
+def _get_norm_layer_type(norm_type, n_dims):
     if norm_type == None:
         return None
     elif norm_type.lower() == "batch":
