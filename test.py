793c5cc60724eb689be8833eaced94dd91eabfcb
Tom Findlay <findlaytel@gmail.com>
2021-08-19 02:18:26 +0100

    fixed import style

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from convo import *
+import convo
 from torch import ones
 
 in_shape = [3,256,256]
 out_shape = [20,1,1]
 
-layers = make_convolutions(in_shape, out_shape, 3, kernel_size=None, 
+layers = convo.make_convolutions(in_shape, out_shape, 3, kernel_size=None, 
         stride=None, padding_mode="zeros", dilation=1, bias=True,
         activation=nn.ReLU, pool_type="max", norm_type=None, module_list=False)
 print(layers)
